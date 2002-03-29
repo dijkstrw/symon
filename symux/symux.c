cd557bd986c00f086417e8e9f27272a6a1423239
Willem Dijkstra <wpd@xs4all.nl>
2002-03-29 16:28:15 +0000

    Added rrd support and daemon mode

--- symux/symux.c
+++ symux/symux.c
@@ -1,5 +1,5 @@
 /*
- * $Id: symux.c,v 1.5 2002/03/29 15:17:55 dijkstra Exp $
+ * $Id: symux.c,v 1.6 2002/03/29 16:28:15 dijkstra Exp $
  *
  * Daemon that multiplexes incoming mon traffic to subscribed clients
  * and archives it periodically in rrd files.
@@ -14,6 +14,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <syslog.h>
+#include <stdio.h>
+#include <rrd.h>
 
 #include "error.h"
 #include "monmux.h"
@@ -50,17 +52,28 @@ int main(argc, argv)
     struct monpacket packet;
     struct packedstream ps;
     struct source *source;
+    struct stream *stream;
 
     char stringbuf[_POSIX2_LINE_MAX];
     int offset;
-
+    char *arg_ra[3];
+    time_t t;
+	
     /* parse configuration file */
     read_config_file("monmux.conf");
+
+#ifndef DEBUG
+    if (daemon(0,0) != 0) {
+	syslog(LOG_ALERT, "daemonize failed -- exiting");
+	fatal("daemonize failed");
+    }
+#endif
+
+    syslog(LOG_INFO, "monmux $Revision: 1.6 $ started");
 
     mux = SLIST_FIRST(&muxlist);
 
     /* catch signals */
-//    signal(SIGALRM, alarmhandler);
     signal(SIGINT, signalhandler);
     signal(SIGQUIT, signalhandler);
     signal(SIGTERM, signalhandler);
@@ -74,20 +87,40 @@ int main(argc, argv)
     atexit(close_listensock);
 
     for (;;) {
-	time_t t;
-	
 	wait_for_packet(listen_sock, &sourcelist, &source, &packet);
-
-	/* print time in packet */
-	t = (time_t) packet.header.timestamp;
-	ctime_r(&t, stringbuf);
-	printf("Packet received with ts=%s", stringbuf);
 
 	offset = 0;
 	while (offset < packet.header.length) {
 	    offset += sunpack(packet.data + offset, &ps);
-	    psdata2strn(&ps, stringbuf, sizeof(stringbuf));
-	    printf("type %d(%s):%s\n", ps.type, ps.args, stringbuf);
+
+	    /* find stream in source */
+	    stream = find_source_stream(source, ps.type, ps.args);
+	    if (stream != NULL) {
+		if (stream->file != NULL) {
+		    /* save if file specified */
+		    arg_ra[0] = "rrd_update";
+		    arg_ra[1] = stream->file;
+
+		    t = (time_t) packet.header.timestamp;
+		    sprintf(stringbuf, "%u", t);
+		    psdata2strn(&ps, stringbuf + strlen(stringbuf), 
+				sizeof(stringbuf) - strlen(stringbuf), 
+				PS2STR_RRD);
+
+		    arg_ra[2] = stringbuf;
+
+#ifdef DEBUG
+		    syslog(LOG_INFO,"%d(%s)='%s'",ps.type,ps.args,stringbuf);
+#else
+		    rrd_update(3,arg_ra);
+
+		    if (rrd_test_error()) {
+			syslog(LOG_INFO,"rrd_update:%s",rrd_get_error());
+			rrd_clear_error();                                                            
+		    }
+#endif
+		}
+	    }
 	}
     }
     /* NOT REACHED */
