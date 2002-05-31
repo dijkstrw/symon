fc6c7c20cf0e87a347be3256df1f15bd80310d15
Willem Dijkstra <wpd@xs4all.nl>
2002-05-31 14:25:00 +0000

    Added testsuite for the sharing routines

---
+++ regress/share/testshare.c
@@ -0,0 +1,40 @@
+/* $Id: */
+
+/*
+ * Test shared memory functions
+ *
+ */
+#include <unistd.h>
+#include "share.h"
+
+int verbose;
+
+int
+main(int argc, char *argv[]) 
+{
+    int i;
+    int j;
+    int clients = 25;
+
+/* test initialization */
+    initshare();
+/* test forking n clients */
+    for (i=0; i<clients; i++)
+	spawn_client();
+    
+/* throughput test */
+    for (i=50000; i > 0; i--) {
+	master_forbidread();
+	for (j=0; j<clients; j++)
+	    printf(".");
+	memset(&shm[2], (i%26)+65, 80);
+	shm[1] = 80;
+	master_allowread();
+	usleep(1);
+    }
+/* kill some clients */
+
+    exit(1);
+
+}
+
