daf1de9c9d0d161533bcad727624fff3c26a6dc9
Willem Dijkstra <wpd@xs4all.nl>
2007-07-05 14:20:06 +0000

    confirmed compile on netbsd4

--- platform/NetBSD/conf.sh
+++ platform/NetBSD/conf.sh
@@ -3,7 +3,8 @@ if [ -f /usr/include/net/pfvar.h ]; then
 else
     echo "#undef HAS_PFVAR_H"
 fi
-case `grep -csq HW_IOSTATS /usr/include/sys/sysctl.h` in
-1)	echo "#define HAS_HW_IOSTATS	1" ;;
-0)	echo "#undef HAS_HW_IOSTATS" ;;
-esac;
+if grep -q HW_IOSTATS /usr/include/sys/sysctl.h; then
+    echo "#define HAS_HW_IOSTATS	1"
+else
+    echo "#undef HAS_HW_IOSTATS"
+fi
