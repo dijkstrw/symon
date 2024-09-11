8c0a6325ae83ea2832ca8c50c1bc1ed88109f80d
Tim Kuijsten <info+git@netsend.nl>
2024-09-11 15:49:06 +0200

    OpenBSD pf: remove ifdef HAS_PFVAR_H
    Depends on pf(4) which was introduced in OpenBSD 3.0 in 2001.

--- platform/OpenBSD/conf.sh
+++ platform/OpenBSD/conf.sh
@@ -2,8 +2,3 @@ case `grep -csq KERN_MBSTAT /usr/include/sys/sysctl.h` in
 1)	echo "#define HAS_KERN_MBSTAT	1" ;;
 0)	echo "#undef HAS_KERN_MBSTAT" ;;
 esac;
-if [ -f /usr/include/net/pfvar.h ]; then
-    echo "#define HAS_PFVAR_H	1"
-else
-    echo "#undef HAS_PFVAR_H"
-fi
